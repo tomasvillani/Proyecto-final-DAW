d5a40605bf1dd723d5817ef62175983b4e5f505e
tomasvillani <tomasvillanidepedro2022@gmail.com>
2025-02-24 13:12:16 +0000

    Inicio

---
+++ resources/js/app.js
@@ -0,0 +1,14 @@
+import './bootstrap';
+import './functions';
+
+import '../css/app.css';
+import '../css/flaticon.css';
+import '../css/owl.carousel.min.css';
+import '../css/bootstrap.min.css';
+import '../css/style.css';
+
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
+
+Alpine.start();
---
+++ resources/js/functions.js
@@ -0,0 +1,12 @@
+import './bootstrap';
+import './counterup.min.js';
+import './easing.min.js';
+import './owl.carousel.min.js';
+import './waypoints.min.js';
+import './main';
+
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
+
+Alpine.start();
